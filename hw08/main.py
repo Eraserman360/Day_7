30db9a9fb90a5935f2a71e76c2501ef34af818b7
Eraserman360 <shiverskij.georgij@mail.ru>
2023-01-30 23:24:31 +0500

    hw08 second task

--- hw08/main.py
+++ hw08/main.py
@@ -1,9 +1,7 @@
+#Задание 1
 import requests
 import json
 
-# response = requests.get("https://jsonplaceholder.typicode.com/todos")
-# todos = json.loads(response.text)
-# print(todos)
 
 all_heroes = requests.get("https://akabab.github.io/superhero-api/api/all.json")
 all_heroes_json = json.loads(all_heroes.text)
@@ -18,3 +16,25 @@ for i in heroes_list:
 
 intelligence_dict = dict([max(intelligence_dict.items())])
 print(f"Самым умным является:{list(intelligence_dict.keys())[0]}")
+
+#Задание 2
+# import requests
+
+def upload_to_yandex_disk(path, token, filename):
+    URL = "https://cloud-api.yandex.net/v1/disk/resources/upload?path="+filename
+
+    headers = {
+        "Accept": "application/json",
+        "Authorization": f"OAuth {token}",
+    }
+    upload_URL = (requests.get(URL, headers=headers)).json()["href"]
+    with open(path, "rb") as f:
+        files = {"file": (filename, f)}
+        response = requests.put(upload_URL, headers=headers, files=files)
+
+    if response.status_code == 201:
+        return response.json()["href"]
+    else:
+        raise Exception("Failed to upload file")
+
+upload_to_yandex_disk("C:\\16700566174011.mp4", "", "16700566174011")
